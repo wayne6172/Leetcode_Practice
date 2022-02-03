5c2990c9e7552785acfdd152016cb3c53bf88617
wayne <wayne8692wayne8692@gmail.com>
2022-02-03 23:36:28 +0800

    solve Q454 (2022/02/03 daily practice)

---
+++ Q454/Go/main.go
@@ -0,0 +1,32 @@
+package main
+
+func neg(x int) int {
+	return x * -1
+}
+
+func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
+	sums1 := make(map[int]int)
+	sums2 := make(map[int]int)
+
+	for index1, _ := range nums1 {
+		for _, value2 := range nums2 {
+			sums1[nums1[index1]+value2]++
+		}
+	}
+
+	for index3, _ := range nums3 {
+		for _, value4 := range nums4 {
+			sums2[nums3[index3]+value4]++
+		}
+	}
+
+	ans := 0
+	for indexS1 := range sums1 {
+		x := sums1[indexS1]
+		y := sums2[neg(indexS1)]
+
+		ans += x * y
+	}
+
+	return ans
+}
